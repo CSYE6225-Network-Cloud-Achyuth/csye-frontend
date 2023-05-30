c9aeea232f50af6c27baae709de2d533b97ee2a3
Achyuth Varma <achyuth.varma1@gmail.com>
2023-05-30 18:29:34 +0530

    Changed to SignUp

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import SignIn from "../../components/SignIn";
+import SignUp from "../../components/SignUp";
 
 const LoginPage = () => {
   return (
     <div className="grid grid-cols-2">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <SignIn />
+        <SignUp />
       </div>
       <h2>Picture</h2>
     </div>
